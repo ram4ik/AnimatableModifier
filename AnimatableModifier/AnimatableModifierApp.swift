6b2d18aafde40333299338a22544e70acba81853
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-17 14:49:12 +0200

    Initial Commit

---
+++ AnimatableModifier/AnimatableModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatableModifierApp.swift
+//  AnimatableModifier
+//
+//  Created by Ramill Ibragimov on 17.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatableModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
