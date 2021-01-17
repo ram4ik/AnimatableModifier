cb42f6498ace66ecfec02c0496c39f6498f1893e
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-17 14:59:10 +0200

    init

--- AnimatableModifier/ContentView.swift
+++ AnimatableModifier/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var percent: CGFloat = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Circle()
+            .fill(Color(.systemGray6))
+            .frame(width: 150, height: 150)
+            .modifier(AnimatingNumberOverlay(number: percent))
+        
+        Button(action: {
+            percent = 0
+            withAnimation(Animation.easeInOut(duration: 1)) {
+                percent = 100
+            }
+        }, label: {
+            Text("Animate")
+        })
+        .accentColor(.primary)
+    }
+}
+
+struct AnimatingNumberOverlay: AnimatableModifier {
+    var number: CGFloat
+    var animatableData: CGFloat {
+        get {
+            number
+        }
+        set {
+            number = newValue
+        }
+    }
+    
+    func body(content: Content) -> some View {
+        return content
+            .overlay(Text("\(Int(number))"))
+            .font(.largeTitle)
     }
 }
 
